d0cc24d68914c9a5efbfe26fbf0b298e28d038ec
vbairam <49372218+BatsmanovMax@users.noreply.github.com>
2021-08-26 13:39:13 +0300

    added filter for items list

--- src/components/IncomeList.js
+++ src/components/IncomeList.js
@@ -1,16 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import IncomeItem from './IncomeItem';
+import getAllUsersList from '../api';
 
 function IncomeList({ income, setIncome }) {
-  
+  const [users, setUsers] = useState(null);
+  // const [fawStatus, setFawStatus] = useState(false)
+
   const sortByDate = (a, b) => {
     return a.date - b.date;
   }
+  const choosedUser = useRef(null);
+
+  const getUsers = async () => {
+    const usersData = await getAllUsersList()
+    setUsers(usersData)
+  }
+
+  const userFilter = () => {
+    var arr = fawFilter(false)
+    arr = (arr.filter((item) => {
+      return item.choosedUser === choosedUser.current.value
+    }))
+    setListItems(arr)
+  }
 
   const fawFilter = (state) => {
+    // setFawStatus(state)
     return income.filter((item, index) => {
       item.id = index
-      return item.isFaw === state }).sort(sortByDate)
+      return item.isFaw === state
+    }).sort(sortByDate)
   }
 
   const [listItems, setListItems] = useState(fawFilter(false))
@@ -24,28 +43,39 @@ function IncomeList({ income, setIncome }) {
     income[i].isFaw = state
     setListItems(fawFilter(!state))
   }
-  
+
+  useEffect(() => {
+    !users && getUsers()
+  }, [users])
+
   return (
-  <>
-  {/* {listItems.length > 0 ? */}
-    <div className="income-list">
-      <button onClick={() => setListItems(() => fawFilter(true))}>favourite posts</button>
-      <button onClick={() => setListItems(() => fawFilter(false))}>normal posts</button>
-      {
-        listItems.map((value, index) => (
-          <IncomeItem
-            key={index} 
-            income={value} 
-            index={value.id} 
-            removeUser={removeUser}
-            markFaw={markFaw}
-          />
-        ))
+    <>
+      {income.length > 0 ?
+        <div className="income-list">
+          <button onClick={() => setListItems(() => fawFilter(true))}>favourite posts</button>
+          <button onClick={() => setListItems(() => fawFilter(false))}>normal posts</button>
+          <select name="users" className="select" id="users" ref={choosedUser} onChange={userFilter}>
+              {users && users.map((user) => {
+                return <option value={user.name} key={user.id}>{user.name}</option>
+              })}
+            </select><br/>
+          {listItems.length > 0 ?
+            listItems.map((value, index) => (
+              <IncomeItem
+                key={index}
+                income={value}
+                index={value.id}
+                removeUser={removeUser}
+                markFaw={markFaw}
+              />
+            ))
+            :
+            <h2>No items here</h2>
+          }
+        </div>
+        : <h2 className="income-list">No tracked time</h2>
       }
-    </div>
-    {/* : <h2>No tracked time</h2> */}
-{/* } */}
-  </>
+    </>
   )
 }
 
